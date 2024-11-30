6541c32e26704ecb652ecc4f8cfa3836128e0983
hanhoudebaga <350359874@qq.com>
2024-11-29 20:29:35 -0500

    session -Huyou

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ import itemRoutes from './routes/items.js';
 import userRoutes from './routes/users.js';
 import tagRoutes from './routes//tags.js';
 import express from 'express';
+import session from 'express-session'
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+    session({
+      name: 'FinalProject',
+      secret: "This is a secret",
+      saveUninitialized: false,
+      resave: false,
+      cookie: {maxAge: 1800000}
+    })
+  );
 app.use('/public', express.static('public'));
 app.use('/item', itemRoutes);
 app.use('/user', userRoutes);
 app.use('/tag', tagRoutes);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use('*', (req, res) => {
     res.status(404).json({ error: '404 Not found' });
 });
